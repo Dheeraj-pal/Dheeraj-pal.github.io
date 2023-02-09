ed3ebff18cfc5c1a1ec524c9b1c53864b2a13e47
Dheeraj-pal <dheeraj.pal19@gmail.com>
2023-02-10 02:33:27 +0530

    pushed

--- script/index.js
+++ script/index.js
@@ -17,8 +17,7 @@ function closeMenu() {
   navMenu.classList.remove("active");
 }
 
-// const cursor = document.querySelector(".cursor");
-// document.addEventListener("mousemove", (e)=>{
-//   cursor.style.left = e.pageX + 'px';
-//   cursor.style.top = e.pageY + 'px';
-// })
+function NewTab() {
+  window.open(
+  "./resume/Dheeraj_pal_Resume.pdf", "_blank");
+}
